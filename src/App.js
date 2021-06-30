7a8286190eacf76a2a9fafec28957a3fdfada503
iqbal <iqbalraju123@gmail.com>
2021-06-30 21:42:32 +0600

    mapping data to component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Card3 from './components/Card3';
+import Data from './components/data.json'
 
 
 function App(){
+  //console.log(Data);
+  
+  //using loop for access all data
+  let items = []
+  /*
+  for(let x=0; x<Data.length; x++){
+    items.push( <Card3 nametext={Data[x].title} proftext={Data[x].description} />)
+  }
+  */
   //props: how to value pass through the component)
-  return <Card3 nametext="my name is iqbal" proftext="i like traveling" />;
+  items = Data.map((items) => <Card3 nametext={items.title} proftext={items.description}/>) //how sent data to component using mapping we can use it instead of for loop
+  console.log(Data)
+  
+  return <div>
+    {items}
+    </div>
 
 }
 
